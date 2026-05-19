69573b38ce549558f60029cd03e223500e387ef5
Peymaan <pmnarimani@gmail.com>
2026-05-19 19:58:17 +0200

    changed the nvim theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -387,23 +387,22 @@ do
   }
 
   -- [[ Colorscheme ]]
-  -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command under that to load whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
+  vim.pack.add { gh 'rebelot/kanagawa.nvim' }
   ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
+  require('kanagawa').setup {
     styles = {
-      comments = { italic = false }, -- Disable italics in comments
+      commentStyle = { italic = false }, -- Disable italics in comments
     },
+
+    overrides = function(colors)
+      return {
+        ['@lsp.type.type.go'] = { fg = colors.theme.syn.type },
+        ['@lsp.type.function.go'] = { fg = colors.theme.syn.fun },
+      }
+    end,
   }
 
-  -- Load the colorscheme here.
-  -- Like many other themes, this one has different styles, and you could load
-  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'kanagawa'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
